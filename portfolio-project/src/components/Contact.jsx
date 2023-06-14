d3d6bb813ced7508d56e0442665febc1a00c2bff
Jason Chan <jason@Jasons-Air.home>
2023-06-14 17:51:38 +0100

    this commit will add text boxes to that contact page

--- portfolio-project/src/components/Contact.jsx
+++ portfolio-project/src/components/Contact.jsx
@@ -1,11 +1,30 @@
 export const Contact = () => {
   return (
-    <section className="pt-5">
+    <section className="pt-5 pb-5">
       <img
         src={require("../avatar/avatar-contact.png")}
         alt="Contact Avatar"
         className="img-fluid"
       />
+      <h2 className="pt-5">Send a Message</h2>
+      <form>
+        <label htmlFor="message">Message</label>
+        <br></br>
+        <textarea
+          type="text"
+          placeholder="Enter Message..."
+          className="p-3 w-25 message-input"
+          id="message"
+        />
+        <br></br>
+        <label htmlFor="email" className="p-3">
+          Email
+        </label>
+        <br></br>
+        <input type="text" className="mb-3" id="email"></input>
+        <br></br>
+        <button type="submit">Send</button>
+      </form>
     </section>
   );
 };
